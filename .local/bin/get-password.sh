a4d2ac4f98fc7b2e49fa6516331aae2760d99298
KlaasJan <klaasjanelzinga@me.com>
2020-07-24 14:36:35 +0200

    pw

--- .local/bin/get-password.sh
+++ .local/bin/get-password.sh
@@ -1,8 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
 SESSION_KEY=$(bw unlock --raw)
+bw sync --session $SESSION_KEY
 
 case $1 in
   "ssh")
     bw get password "ssh private key thinkpad" --session $SESSION_KEY | xclip -selection c
+    ;;
+  "a2bc")
+    bw get password portal.a2bc.com --session $SESSION_KEY | xclip -selection c
+    ;;
+  "discover")
+    bw get password discover.movingspirits.nl --session $SESSION_KEY | xclip -selection c
     ;;
   "portal")
     bw get password portal.movingspirits.nl --session $SESSION_KEY | xclip -selection c
