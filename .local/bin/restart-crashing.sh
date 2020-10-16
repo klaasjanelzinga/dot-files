7189b1f33e047d9869bb062f94af2e546059faa9
KlaasJan <klaasjanelzinga@me.com>
2020-10-16 09:22:39 +0200

    wallpaper

--- .local/bin/restart-crashing.sh
+++ .local/bin/restart-crashing.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/bash
 
+export DISPLAY=:0
 pcloud=$(ps aux | grep -i pcloud | grep -v grep | wc -l)
 if [ $pcloud -eq 0 ]; then
-  exec pcloud 
+  i3-msg 'exec pcloud'
 fi
 
 slack=$(ps aux | grep -i slack | grep -v grep | wc -l)
 if [ $slack -eq 0 ]; then
-  exec i3-msg 'workspace 6; exec slack'
+  i3-msg 'exec slack'
+  sleep 3
+  i3-msg '[class="Slack"] move to workspace "6"'
 fi
 
 teams=$(ps aux | grep -i teams | grep -v grep | wc -l)
 if [ $teams -eq 0 ]; then
-  exec i3-msg 'workspace 6; exec teams'
+  i3-msg 'exec teams'
+  sleep 5
+  i3-msg '[class="Microsoft Teams - Preview"] move to workspace "6"'
 fi
