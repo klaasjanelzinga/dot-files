c04b4f1dde041ada99e33870f1676b23779f4963
KlaasJan Elzinga <klaasjanelzinga@me.com>
2020-10-22 10:44:14 +0200

    update

--- .local/bin/wallpaper-start.sh
+++ .local/bin/wallpaper-start.sh
@@ -3,7 +3,7 @@
 while [ 0 ]; do
   file=$(find ~/pCloudDrive/klaasjan/pics/desktop/*.png |sort -R |tail -1)
   feh -q --bg-scale $file
-  sleep 180
+  sleep 360
 done
 # export I3SOCK=$(ls /run/user/1000/sway-ipc.1000.*.sock)
 # swaymsg output "*"  bg $file fill
