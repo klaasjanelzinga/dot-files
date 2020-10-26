2ef6cbc13a5323d7710bb31179c9deff9f99453e
KlaasJan Elzinga <klaasjanelzinga@me.com>
2020-10-26 20:05:59 +0100

    single wallpaper script

--- .local/bin/dpms.sh
+++ .local/bin/dpms.sh
@@ -7,10 +7,9 @@ case $new_level in
   # reset to defaults and start timers
   dpms.sh 
   xidlehook --socket ~/.local/xidlehook.socket \
-	--timer 60  "dpms.sh 3" "dpms.sh" \
-	--timer 220 "dpms.sh 2" "dpms.sh" \
-	--timer 240 "dpms.sh 1" "dpms.sh" \
-	--timer 900 "dpms.sh 0" "dpms.sh"
+		--timer 60  "dpms.sh 3" "dpms.sh" \
+		--timer 240 "dpms.sh 1" "dpms.sh" \
+		--timer 900 "dpms.sh 0" "dpms.sh"
   ;;
 "stop")
   pkill xidlehook
@@ -37,12 +36,12 @@ case $new_level in
 3)
   xrandr --output HDMI2 --brightness .25
   brightnessctl -q s 15%
-  wallpaper-stop.sh
+  wallpaper.sh stop
   ;;
 *)
   xrandr --output HDMI2 --brightness 1
   brightnessctl -q s 60%
-  nohup wallpaper-start.sh </dev/null >~/.local/wallpaper-log 2>&1 &
+  nohup wallpaper.sh start </dev/null >~/.local/wallpaper-log 2>&1 &
   ;;
 esac
 
--- .local/bin/wallpaper-stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kill $(ps aux | grep wallpaper-start.sh | grep -v grep | awk '{ print $2 }')
-
